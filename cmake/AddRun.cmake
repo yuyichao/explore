2205d703a3f7581096e2990e55259d86ece611cc
Yichao Yu <yyc1992@gmail.com>
2012-06-26 10:22:21 +0800

    add_run

---
+++ cmake/AddRun.cmake
@@ -0,0 +1,12 @@
+function(add_run run_name)
+  set(options)
+  set(oneValueArgs "WORKING_DIRECTORY")
+  set(multiValueArgs "COMMAND")
+  cmake_parse_arguments(ADD_RUN "${options}"
+    "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  set(args COMMAND ${ADD_RUN_COMMAND})
+  if (ADD_RUN_WORKING_DIRECTORY)
+    set(args ${args} WORKING_DIRECORY ${ADD_RUN_WORKING_DIRECTORY})
+  endif()
+  add_custom_target(${run_name} ${args})
+endfunction()
--- cmake/TestWrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec &> /dev/tty
-exec "$@"

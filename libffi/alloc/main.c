7b51127ba78ded545618a748bdbba647c43e1396
Yichao Yu <yyc1992@gmail.com>
2012-06-27 21:17:50 +0800

    -

--- libffi/alloc/main.c
+++ libffi/alloc/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ffi.h>
 #include <string.h>
+#include <stdarg.h>
 
 typedef void (*fpointer)();
 
@@ -76,6 +77,71 @@ ffi_type *type_copy(ffi_type *type)
     size = type_fill_buff(buff, type);
     printf("copy size: %u\n", size);
     return buff;
+}
+
+ffi_type*
+type_create(unsigned int argc, ...)
+{
+    if (!argc)
+        return NULL;
+    int i;
+    va_list ap;
+    ffi_type *eles[argc + 1];
+    ffi_type tmpres = {
+        .size = 0,
+        .alignment = 0,
+        .elements = eles
+    };
+    va_start(ap, argc);
+    for (i = 0;i < argc;i++) {
+        eles[i] = va_arg(ap, ffi_type*);
+    }
+    va_end(ap);
+    eles[i] = NULL;
+    return type_copy(&tmpres);
+}
+
+ffi_cif*
+cif_create(ffi_type *ret_t, unsigned int argc, ...)
+{
+    unsigned int i;
+    unsigned int size;
+    ffi_cif *res;
+    va_list ap;
+
+    ffi_type *argv[argc];
+    void *ret_p;
+    void *argv_p;
+    void *arg_p;
+
+    size = sizeof(ffi_cif);
+    if (!ret_t)
+        ret_t = &ffi_type_void;
+    size += type_total_size(ret_t) + sizeof(argv);
+
+    va_start(ap, argc);
+    for (i = 0;i < argc;i++) {
+        argv[i] = va_arg(ap, ffi_type*);
+        size += type_total_size(argv[i]);
+    }
+    va_end(ap);
+
+    res = malloc(size);
+    if (!res)
+        return NULL;
+    ret_p = res + sizeof(ffi_cif);
+    argv_p = ret_p + type_fill_buff(ret_p, ret_t);
+    arg_p = argv_p + sizeof(argv);
+    for (i = 0;i < argc;i++) {
+        ((ffi_cif**)argv_p)[i] = arg_p;
+        arg_p += type_fill_buff(arg_p, argv[i]);
+    }
+    if (!argc)
+        argv_p = NULL;
+    if (ffi_prep_cif(res, FFI_DEFAULT_ABI, argc, ret_p, argv_p) == FFI_OK)
+        return res;
+    free(res);
+    return NULL;
 }
 
 typedef struct {
@@ -153,50 +219,36 @@ print_cif(ffi_cif *cif)
 int
 main()
 {
-    ffi_cif cif;
-    ffi_type my_struct_type;
-    ffi_type *my_struct_eles[4] = {
-        &ffi_type_sint,
-        &ffi_type_pointer,
-        &ffi_type_sint,
-        NULL
-    };
-    ffi_type *args[1];
+    ffi_cif *cif;
     void *values[1];
     my_struct ms = {
         .i = 1,
         .j = 2,
         .str = "asdf"
     };
-
-    my_struct_type.size = my_struct_type.alignment = 0;
-    my_struct_type.elements = my_struct_eles;
-
-    ffi_type *copied_type = type_copy(&my_struct_type);
-
-    args[0] = copied_type;
     values[0] = &ms;
 
+    ffi_type *copied_type = type_create(3, &ffi_type_sint, &ffi_type_pointer,
+                                        &ffi_type_sint);
     printf("\033[31;1mBefore Init\033[0m\n");
     printf("my_struct_type:\n");
-    print_type(args[0]);
-    printf("ffi_type_void:\n");
-    print_type(&ffi_type_void);
+    print_type(copied_type);
 
-    if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 1,
-                     &ffi_type_void, args) == FFI_OK) {
+    cif = cif_create(NULL, 1, copied_type);
+    free(copied_type);
+    if (cif) {
         printf("\033[31;1mAfter Prep CIF\033[0m\n");
-        print_cif(&cif);
+        print_cif(cif);
         printf("\033[32;1mCall First Time\033[0m\n");
-        ffi_call(&cif, (fpointer)print_my_struct, NULL, values);
+        ffi_call(cif, (fpointer)print_my_struct, NULL, values);
         printf("\033[31;1mAfter First Call\033[0m\n");
-        print_cif(&cif);
+        print_cif(cif);
         ms.str = "Hello";
         printf("\033[32;1mCall Second Time\033[0m\n");
-        ffi_call(&cif, (fpointer)print_my_struct, NULL, values);
+        ffi_call(cif, (fpointer)print_my_struct, NULL, values);
         printf("\033[31;1mAfter Second Call\033[0m\n");
-        print_cif(&cif);
+        print_cif(cif);
     }
-    free(copied_type);
+    free(cif);
     return 0;
 }

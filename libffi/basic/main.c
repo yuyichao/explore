c2a2aca96a9899375c1bf3008a2fbfe11d8b1b33
Yichao Yu <yyc1992@gmail.com>
2012-06-27 14:53:55 +0800

    -

--- libffi/basic/main.c
+++ libffi/basic/main.c
@@ -13,6 +13,41 @@ print_str(char *str)
         printf("(null)\n");
 }
 
+void
+print_type(ffi_type *type)
+{
+    if (!type) {
+        printf("Empty type.\n");
+        return;
+    }
+    printf("type @ %lx\n", (unsigned long)type);
+    printf("\tsize: %lu\n", type->size);
+    printf("\talignment: %u\n", (unsigned)type->alignment);
+    printf("\ttype: %u\n", (unsigned)type->type);
+}
+
+void
+print_cif(ffi_cif *cif)
+{
+    int i;
+    if (!cif) {
+        printf("Empty cif.\n");
+        return;
+    }
+    printf("cif @ %lx\n", (unsigned long)cif);
+    printf("nargs: %u\n", cif->nargs);
+    printf("bytes: %u\n", cif->bytes);
+    printf("flags: %u\n", cif->flags);
+    printf("rtype:");
+    print_type(cif->rtype);
+    printf("arg_types:\n");
+    for (i = 0;i < cif->nargs;i++) {
+        printf("args[%d]:\n", i);
+        print_type(cif->arg_types[i]);
+    }
+    printf("\n");
+}
+
 int
 main()
 {
@@ -23,13 +58,20 @@ main()
 
     args[0] = &ffi_type_pointer;
     values[0] = &s;
+    printf("ffi_type_pointer:\n");
+    print_type(args[0]);
+    printf("ffi_type_void:\n");
+    print_type(&ffi_type_void);
 
     if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 1,
                      &ffi_type_void, args) == FFI_OK) {
+        print_cif(&cif);
         s = "aaa";
         ffi_call(&cif, (fpointer)print_str, NULL, values);
+        print_cif(&cif);
         s = NULL;
         ffi_call(&cif, (fpointer)print_str, NULL, values);
+        print_cif(&cif);
     }
     return 0;
 }

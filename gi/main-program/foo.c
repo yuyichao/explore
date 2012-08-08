1b93e4abea1ff587fec264cf9a68f94c476b47a1
Yichao Yu <yyc1992@gmail.com>
2012-08-08 19:37:45 -0400

    more python test

--- gi/main-program/foo.c
+++ gi/main-program/foo.c
@@ -8,7 +8,7 @@
 
 /**
  * foo_hello:
- * @str: (allow-none) (tranfer none):
+ * @str: (allow-none) (transfer none):
  *
  * test
  */
@@ -25,16 +25,18 @@ main(int argc, char **argv)
     Py_Initialize();
     char *js_path[2] = { 0 };
     js_path[0] = getenv("JS_PATH");
+    wchar_t *v[] = {};
 
     GOptionContext *ctx = g_option_context_new(NULL);
     g_option_context_add_group(ctx, g_irepository_get_option_group());
     g_option_context_parse(ctx, &argc, &argv, NULL);
+    PySys_SetArgv(0, v);
 
     PyRun_SimpleString("from time import time, ctime\n"
                        "print('Today is', ctime(time()))\n");
     GjsContext *gjs_ctx = gjs_context_new_with_search_path(js_path);
     gjs_context_eval(gjs_ctx, "const Main = imports.main; Main.start();",
-                      -1, "<main>", NULL, NULL);
+                     -1, "<main>", NULL, NULL);
     PyRun_SimpleString("from gi.repository import Foo\n"
                        "Foo.hello('python')\n");
     FILE *py_file;
--- gi/main-program/main.py
+++ gi/main-program/main.py
@@ -1,6 +1,10 @@
 from gi.repository import Foo
+import sys
 
 Foo.hello(str(Foo))
 Foo.hello("from, %s" % __file__)
+print(sys.executable)
+print(sys.argv)
+print(__name__)
 
 print(Foo.hello)

883615adf791337db2a5484b3815b3cc3a9b5bd8
Yichao Yu <yyc1992@gmail.com>
2015-05-30 22:08:32 -0400

    [julia/throw_error] more realistic example

--- julia/throw_error/throw_error.jl
+++ julia/throw_error/throw_error.jl
@@ -13,38 +13,34 @@ function time_func(f::Function, args...)
 end
 
 function f0(a, b)
-    if b == 0
+    if b === 0
         a
     end
     a
 end
 
 function f1(a, b)
-    if b == 0
+    if b === 0
         throw1(BoundsError, a, b)
     end
     a
 end
 
 function f2(a, b)
-    if b == 0
+    if b === 0
         throw(BoundsError(a, b))
     end
     a
 end
 
-@code_llvm f0([], 1)
-@code_llvm f1([], 1)
-@code_llvm f2([], 1)
-
 @code_llvm f0(0, 1)
 @code_llvm f1(0, 1)
 @code_llvm f2(0, 1)
 
-time_func(f0, [], 1)
-time_func(f1, [], 1)
-time_func(f2, [], 1)
+v = Base.svec(1, 2)
+@code_llvm getindex(v, 1)
 
 time_func(f0, 0, 1)
 time_func(f1, 0, 1)
 time_func(f2, 0, 1)
+time_func(getindex, v, 1)

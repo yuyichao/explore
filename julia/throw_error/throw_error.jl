bba667705869d9f8ef52690bfd9edb045d222d05
Yichao Yu <yyc1992@gmail.com>
2015-06-04 11:37:30 -0400

    [throw_error] add interpolation in error

--- julia/throw_error/throw_error.jl
+++ julia/throw_error/throw_error.jl
@@ -1,6 +1,12 @@
 #!/usr/bin/julia -f
 
+macro throw_interp(typ, arg1, arg2)
+    :(throw(typ(string("Random format string, ", $arg1, ", ", $arg2))))
+end
+
 @noinline throw1(typ::ANY, arg1, arg2) = throw(typ(arg1, arg2))
+@noinline throw_interp(typ::Any, arg1, arg2) =
+    @throw_interp(typ, arg1, arg2)
 
 function time_func(f::Function, args...)
     println(f)
@@ -33,9 +39,25 @@ function f2(a, b)
     a
 end
 
+function f3(a, b)
+    if b === 0
+        throw_interp(ArgumentError, a, b)
+    end
+    a
+end
+
+function f4(a, b)
+    if b === 0
+        @throw_interp(ArgumentError, a, b)
+    end
+    a
+end
+
 @code_llvm f0(0, 1)
 @code_llvm f1(0, 1)
 @code_llvm f2(0, 1)
+@code_llvm f3(0, 1)
+@code_llvm f4(0, 1)
 
 v = Base.svec(1, 2)
 @code_llvm getindex(v, 1)
@@ -43,4 +65,6 @@ v = Base.svec(1, 2)
 time_func(f0, 0, 1)
 time_func(f1, 0, 1)
 time_func(f2, 0, 1)
+time_func(f3, 0, 1)
+time_func(f4, 0, 1)
 time_func(getindex, v, 1)

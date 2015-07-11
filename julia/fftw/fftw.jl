0cf117886a7f7726fee2f54ccf2a632b5b2f6191
Yichao Yu <yyc1992@gmail.com>
2015-07-11 16:21:49 -0400

    [julia/fftw] support new fft api

--- julia/fftw/fftw.jl
+++ julia/fftw/fftw.jl
@@ -1,17 +1,34 @@
 #!/usr/bin/julia -f
 
-function test_fft(ary)
-    println(length(ary))
-    plan = plan_fft!(copy(ary), 1:1, FFTW.MEASURE)
-    iplan = plan_ifft!(copy(ary), 1:1, FFTW.MEASURE)
-    plan(ary)
-    gc()
-    @time for i in 1:10_000
+if isdefined(Base, :DFT)
+    function test_fft(ary)
+        println(length(ary))
+        cary = copy(ary)
+        plan = plan_fft!(cary; flags=FFTW.MEASURE)
+        iplan = plan_ifft!(cary, 1:1; flags=FFTW.MEASURE)
+        plan * ary
+        iplan * ary
+        gc()
+        @time for i in 1:10_000
+            plan * ary
+            iplan * ary
+        end
+    end
+else
+    function test_fft(ary)
+        println(length(ary))
+        plan = plan_fft!(copy(ary), 1:1, FFTW.MEASURE)
+        iplan = plan_ifft!(copy(ary), 1:1, FFTW.MEASURE)
         plan(ary)
         iplan(ary)
+        gc()
+        @time for i in 1:10_000
+            plan(ary)
+            iplan(ary)
+        end
     end
 end
 
-for s in (10, 100, 10000, 16, 128, 16384)
+for s in (10, 10, 100, 10000, 16, 128, 16384)
     test_fft(rand(Complex{Float64}, s))
 end

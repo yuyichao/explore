16418a74bde67f65ec5d0d258745c37b4ca4c9d4
Yichao Yu <yyc1992@gmail.com>
2015-07-30 11:48:21 -0400

    [julia/simd_complex] load hoisting for mutable type

--- julia/simd_complex/simd_complex2.jl
+++ julia/simd_complex/simd_complex2.jl
@@ -2,16 +2,21 @@
 
 using SoArrays
 
+immutable A{T1,T2}
+    a::T1
+    b::T2
+end
+
 function f1(x, a)
     s = real(zero(eltype(x)))
-    @inbounds for i in 1:size(x, 1)
+    @inbounds @simd for i in 1:size(x, 1)
         x1 = x[i, 1]
         x2 = x[i, 2]
-        x1 *= a
+        x1 *= a.a
         b = x1 + x2
         s += abs2(b)
         x[i, 1] = x1
-        x[i, 2] = x2 * a
+        x[i, 2] = x2 * a.a
     end
     s
 end
@@ -22,14 +27,15 @@ end
 
 function f2(x, a)
     s = real(zero(eltype(x)))
+    _a = a.a
     @inbounds @simd for i in 1:size(x, 1)
         x1 = x[i, 1]
         x2 = x[i, 2]
-        x1 *= a
+        x1 *= _a
         b = x1 + x2
         s += abs2(b)
         x[i, 1] = x1
-        x[i, 2] = x2 * a
+        x[i, 2] = x2 * _a
     end
     s
 end
@@ -42,22 +48,25 @@ r = reshape(Float32[1:2048;], (1024, 2))
 c = reshape(Complex64[1:2048;], (1024, 2))
 sc = convert(SoArray, c)
 
-@time g1(1, r, 1.0f0)
-@time g1(1, c, 1.0f0im)
-@time g1(1, sc, 1.0f0im)
+ra = A(1.0f0, Type)
+ca = A(1.0f0im, Type)
 
-@time g2(1, r, 1.0f0)
-@time g2(1, c, 1.0f0im)
-@time g2(1, sc, 1.0f0im)
+@time g1(1, r, ra)
+@time g1(1, c, ca)
+@time g1(1, sc, ca)
+
+@time g2(1, r, ra)
+@time g2(1, c, ca)
+@time g2(1, sc, ca)
 
 println()
 
-@time g1(100_000, r, 1.0f0)
-@time g1(100_000, c, 1.0f0im)
-@time g1(100_000, sc, 1.0f0im)
+@time g1(100_000, r, ra)
+@time g1(100_000, c, ca)
+@time g1(100_000, sc, ca)
 
-@time g2(100_000, r, 1.0f0)
-@time g2(100_000, c, 1.0f0im)
-@time g2(100_000, sc, 1.0f0im)
+@time g2(100_000, r, ra)
+@time g2(100_000, c, ca)
+@time g2(100_000, sc, ca)
 
-# @code_llvm f2(sc, 1.0f0im)
+# @code_llvm f2(sc, ca)

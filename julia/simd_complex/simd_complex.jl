79aa0f7205d6d14a93c34e126b142af33d9bbcf0
Yichao Yu <yyc1992@gmail.com>
2015-07-29 21:53:08 -0400

    [julia/simd_complex] use SoArrays

--- julia/simd_complex/simd_complex.jl
+++ julia/simd_complex/simd_complex.jl
@@ -1,9 +1,12 @@
 #!/usr/bin/julia -f
 
+using SoArrays
+
 function f1(x, a)
     s = zero(eltype(x))
     for i in 1:length(x)
-        @inbounds s += x[i] * a
+        @inbounds b = x[i] * a
+        s += b * b
     end
     s
 end
@@ -15,7 +18,8 @@ end
 function f2(x, a)
     s = zero(eltype(x))
     @simd for i in 1:length(x)
-        @inbounds s += x[i] * a
+        @inbounds b = x[i] * a
+        s += b * b
     end
     s
 end
@@ -24,60 +28,26 @@ g2(n, x, a) = for i in 1:n
     f2(x, a)
 end
 
-function f3{T}(x::AbstractVector{Complex{T}}, a)
-    sr = T(0)
-    si = T(0)
-    ar = real(a)
-    ai = imag(a)
-    len = length(x)
-    r_x = reinterpret(T, x)
-    @simd for i in 0:len-1
-        @inbounds begin
-            vr = r_x[2i + 1]
-            vi = r_x[2i + 2]
-            sr += vr * ar - vi * ai
-            si += vr * ai + vi * ar
-        end
-    end
-    Complex(sr, si)
-end
-
-# function f3{T}(x::AbstractVector{Complex{T}}, a)
-#     sr = T(0)
-#     si = T(0)
-#     ar = real(a)
-#     ai = imag(a)
-#     len = length(x)
-#     r_x = reinterpret(T, x)
-#     @inbounds @simd for i in 1:2len
-#         v = r_x[i]
-#         sr += v * ifelse(i % 2 != 0, ar, -ai)
-#         # si += v * ifelse(i % 2 != 0, ai, ar)
-#     end
-#     Complex(sr, si)
-# end
-
-g3(n, x, a) = for i in 1:n
-    f3(x, a)
-end
-
 r = Float32[1:1024;]
 c = Complex64[1:1024;]
+sc = convert(SoArray, c)
 
 @time g1(1, r, 1.0f0)
 @time g1(1, c, 1.0f0im)
+@time g1(1, sc, 1.0f0im)
 
 @time g2(1, r, 1.0f0)
 @time g2(1, c, 1.0f0im)
+@time g2(1, sc, 1.0f0im)
 
-@time g3(1, c, 1.0f0im)
+println()
 
 @time g1(100_000, r, 1.0f0)
 @time g1(100_000, c, 1.0f0im)
+@time g1(100_000, sc, 1.0f0im)
 
 @time g2(100_000, r, 1.0f0)
 @time g2(100_000, c, 1.0f0im)
+@time g2(100_000, sc, 1.0f0im)
 
-@time g3(100_000, c, 1.0f0im)
-
-# @code_llvm f3(c, 1.0f0im)
+# @code_llvm f2(sc, 1.0f0im)

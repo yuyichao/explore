f27b8a5faf5f520f8c8ab79653c1836054de881d
Yichao Yu <yyc1992@gmail.com>
2015-09-07 08:46:39 -0400

    [julia/issue-12992] Also test multiple close

--- julia/issue-12992/issue-12992.jl
+++ julia/issue-12992/issue-12992.jl
@@ -12,5 +12,25 @@ function test()
     println(1)
 end
 
+function test2()
+    pfw = PollingFileWatcher(@__FILE__, 0.01)
+    close(pfw)
+    close(pfw)
+    pfw = PollingFileWatcher(@__FILE__, 0.01)
+    close(pfw)
+    close(pfw)
+    pfw = PollingFileWatcher(@__FILE__, 0.01)
+    close(pfw)
+    close(pfw)
+    gc()
+    gc()
+    println(2)
+end
+
 test()
 test()
+test()
+
+test2()
+test2()
+test2()

6f451a8055a385d8f9ae7d877f0de72ead79d18b
Yichao Yu <yyc1992@gmail.com>
2015-09-23 09:07:08 -0400

    [julia/soa_aos_copy] cilk_for

--- julia/soa_aos_copy/complex_copy.c
+++ julia/soa_aos_copy/complex_copy.c
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <cilk/cilk.h>
 
 void
 copy_soa2aos(float *__restrict__ src, float *__restrict__ dest1,
@@ -23,8 +24,7 @@ copy_soa2aos_cilk(float *__restrict__ src, float *__restrict__ dest1,
     src = (float*)__builtin_assume_aligned(src, 16);
     dest1 = (float*)__builtin_assume_aligned(dest1, 16);
     dest2 = (float*)__builtin_assume_aligned(dest2, 16);
-#pragma simd
-    for (size_t i = 0;i < len;i++) {
+    cilk_for (size_t i = 0;i < len;i++) {
         dest1[i] = src[i * 2];
         dest2[i] = src[i * 2 + 1];
     }

3805e1694be12efad8eda621baf25f1a16f167dc
Yichao Yu <yyc1992@gmail.com>
2015-09-23 13:31:33 -0400

    [julia/simd_complex] orignal version should work again

--- julia/simd_complex/test.jl
+++ julia/simd_complex/test.jl
@@ -8,15 +8,15 @@ function test_scale2(ary1, ary2, factor)
     end
 end
 
-function test_scale2(ary1, ary2, factor::Complex)
-    fr = real(factor)
-    fi = imag(factor)
-    @inbounds @simd for i in 1:length(ary1)
-        v = Complex(ary1[i], ary2[i]) * Complex(fr, fi)
-        ary1[i] = real(v)
-        ary2[i] = imag(v)
-    end
-end
+# function test_scale2(ary1, ary2, factor::Complex)
+#     fr = real(factor)
+#     fi = imag(factor)
+#     @inbounds @simd for i in 1:length(ary1)
+#         v = Complex(ary1[i], ary2[i]) * Complex(fr, fi)
+#         ary1[i] = real(v)
+#         ary2[i] = imag(v)
+#     end
+# end
 
 Base.code_llvm_raw(test_scale2, Tuple{Vector{Float32}, Vector{Float32}, Complex64})
 # Base.code_llvm_raw(test_scale2, Tuple{Vector{Float32}, Vector{Float32}, Float32})

a9e93d78cb16973afe645197466a7d026cf19fdd
Yichao Yu <yyc1992@gmail.com>
2015-10-06 11:13:44 -0400

    [julia/simd_fill] compare to base

--- julia/simd_fill/simd_fill.jl
+++ julia/simd_fill/simd_fill.jl
@@ -16,6 +16,10 @@ time_normal(A, v, n) = @time for i in 1:n
     fill_normal(A, v)
 end
 
+time_base(A, v, n) = @time for i in 1:n
+    fill!(A, v)
+end
+
 A32 = rand(Float32, 1000, 1000)
 A64 = rand(Float64, 1000, 1000)
 
@@ -46,3 +50,8 @@ time_normal(A32, (0.1f0,), 1000)
 time_simd(A32, (0.1f0,), 1000)
 time_normal(A64, (0.1f0,), 1000)
 time_simd(A64, (0.1f0,), 1000)
+println()
+time_base(A32, 0.1, 1000)
+time_base(A64, 0.1, 1000)
+time_base(A32, 0.1f0, 1000)
+time_base(A64, 0.1f0, 1000)

2659b6c97cbf40b0320c2274908b28ec4587816b
Yichao Yu <yyc1992@gmail.com>
2015-10-11 08:05:25 -0400

    [julia/sin_complex] more typo...

--- julia/sin_complex/sin_complex.jl
+++ julia/sin_complex/sin_complex.jl
@@ -9,7 +9,7 @@ function sin2{T<:AbstractFloat}(z::Complex{T})
     if !isfinite(zr) && !isfinite(zi) return Complex(zr, oftype(zi, NaN)) end
     Complex(sin(zr)*cosh(zi), cos(zr)*sinh(zi))
 end
-sin2(z::Complex) = sin2(float(z))
+@noinline sin2(z::Complex) = sin2(float(z))
 
 function sin3{T<:AbstractFloat}(z::Complex{T})
     zr, zi = reim(z)
@@ -21,17 +21,25 @@ function sin3{T<:AbstractFloat}(z::Complex{T})
     Complex(sin(zr)*cosh(zi), cos(zr)*sinh(zi))
 end
 sin3(z::Complex) = sin3(float(z))
-function sin3{T<:Integer}(z::Complex{T})
-    zr, zi = reim(z)
+@noinline function sin3{T<:Integer}(z::Complex{T})
+    _zr, _zi = reim(z)
+    zr = float(_zr)
+    zi = float(_zi)
     Complex(sin(zr) * cosh(zi), cos(zr) * sinh(zi))
 end
 
-time2(v, n) = @time for i in 1:n
-    sin2(v)
+function time2(v, n)
+    gc()
+    @time for i in 1:n
+        sin2(v)
+    end
 end
 
-time3(v, n) = @time for i in 1:n
-    sin2(v)
+function time3(v, n)
+    gc()
+    @time for i in 1:n
+        sin3(v)
+    end
 end
 
 time2(1im, 1)
@@ -39,16 +47,16 @@ time3(1im, 1)
 time2(big(1im), 1)
 time3(big(1im), 1)
 
-@code_llvm sin2(1im)
-@code_llvm sin3(1im)
+@code_llvm sin2(big(1im))
+@code_llvm sin3(big(1im))
 
 println()
 time2(1im, 100000000)
-time3(1im, 100000000)
 time2(1im, 100000000)
+time3(1im, 100000000)
 time3(1im, 100000000)
 println()
 time2(big(1im), 500000)
-time3(big(1im), 500000)
 time2(big(1im), 500000)
+time3(big(1im), 500000)
 time3(big(1im), 500000)

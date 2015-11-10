767c75129f4b26a493cd2ee59167b2bf5bbe180d
Yichao Yu <yyc1992@gmail.com>
2015-11-10 10:40:34 -0500

    [julia/bench_simple] make sure the small array is cacheline aligned

--- julia/bench_simple/benchmark.jl
+++ julia/bench_simple/benchmark.jl
@@ -47,15 +47,20 @@ function scale1(b, a, s)
     end
 end
 
-ary32_small = rand(Float32, 1024)
-ary64_small = rand(Float64, 1024)
+function get_aligned_rand{T}(::Type{T}, n)
+    while true
+        ary = rand(T, n)
+        Int(pointer(ary)) % 64 == 0 && return ary
+    end
+end
+
+ary32_small = get_aligned_rand(Float32, 1024)
+ary64_small = get_aligned_rand(Float64, 1024)
+ary32_small2 = get_aligned_rand(Float32, 1024)
+ary64_small2 = get_aligned_rand(Float64, 1024)
 
 ary32_big = rand(Float32, 8 * 1024^2)
 ary64_big = rand(Float64, 8 * 1024^2)
-
-ary32_small2 = copy(ary32_small)
-ary64_small2 = copy(ary64_small)
-
 ary32_big2 = copy(ary32_big)
 ary64_big2 = copy(ary64_big)
 

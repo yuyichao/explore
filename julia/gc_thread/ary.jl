d62ec5717e6f2362bdefb26f1e184bcd0227ee29
Yichao Yu <yyc1992@gmail.com>
2015-12-10 21:48:58 -0500

    [julia/gc_thread] array segfault solved

--- julia/gc_thread/ary.jl
+++ julia/gc_thread/ary.jl
@@ -3,10 +3,14 @@
 using Base.Threads
 
 function gen_all_symbols()
-    ary = Vector{Any}(1000_000)
-    for i in 1:1_000_000
-        Array(UInt8, 3)
-    end
+    # ary = Vector{Any}(2000_000)
+    # ary = Vector{Any}(1000_000)
+    ary = ccall(:jl_alloc_cell_1d, Ref{Vector{Any}}, (Csize_t,), 1000_000)
+    # ptr = Ptr{Ptr{Void}}(pointer_from_objref(ary))
+    # ccall(:printf, Cint, (Ptr{Cchar}, Cint, Ptr{Void}, Ptr{Void}),
+    #       "%d, %p, %p\n",
+    #       threadid(), ptr, unsafe_load(ptr - 8))
+    gc(false)
     ary
 end
 

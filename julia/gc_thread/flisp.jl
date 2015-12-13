e8a26c205dc04a8cc93ff8c8419df333b4e1248e
Yichao Yu <yyc1992@gmail.com>
2015-12-12 19:57:05 -0500

    [julia/gc_thread/flisp] remove debug output

--- julia/gc_thread/flisp.jl
+++ julia/gc_thread/flisp.jl
@@ -2,51 +2,32 @@
 
 using Base.Threads
 
-const lock1 = Threads.RecursiveSpinLock()
-const lock2 = Threads.RecursiveSpinLock()
+const lock = Threads.RecursiveSpinLock()
 
 macro macro1()
-    ccall(:printf, Cint, (Cstring, Cint), "%d: macro 1\n", threadid())
-    lock!(lock1)
-    unlock!(lock1)
+    lock!(lock)
+    unlock!(lock)
     nothing
 end
 
 macro macro2()
-    ccall(:printf, Cint, (Cstring, Cint), "%d: macro 2\n", threadid())
     nothing
 end
 
 function f()
     if rand(Bool)
-        ccall(:printf, Cint, (Cstring, Cint), "%d: path1\n",
-              threadid())
-        lock!(lock1)
-        ccall(:printf, Cint, (Cstring, Cint), "%d: locked 1\n",
-              threadid())
+        lock!(lock)
         ex = parse("@macro2")
         ex2 = macroexpand(ex)
         eval(ex2)
-        ccall(:printf, Cint, (Cstring, Cint), "%d: eval'd 1\n",
-              threadid())
-        unlock!(lock1)
-        ccall(:printf, Cint, (Cstring, Cint), "%d: unlocked 1\n",
-              threadid())
+        unlock!(lock)
     else
-        ccall(:printf, Cint, (Cstring, Cint), "%d: path2\n",
-              threadid())
         ex = parse("@macro1")
         ex2 = macroexpand(ex)
         eval(ex2)
-        ccall(:printf, Cint, (Cstring, Cint), "%d: eval'd 2\n",
-              threadid())
     end
 end
 
 @threads for i in 1:100
-    ccall(:printf, Cint, (Cstring, Cint, Cint), "%d: start %d\n",
-          threadid(), i)
     f()
-    ccall(:printf, Cint, (Cstring, Cint, Cint), "%d: finish %d\n",
-          threadid(), i)
 end

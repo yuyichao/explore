5bed64c5365dd5de65fedef52de6edceef12b8f0
Yichao Yu <yyc1992@gmail.com>
2016-04-15 18:45:55 -0400

    [julia/issue-15541] benchmarks for slow stuff

---
+++ julia/issue-15541/doit.jl
@@ -0,0 +1,32 @@
+#!/usr/bin/julia -f
+
+@noinline function bare_gc(n::Int)
+    for s = 1:n
+        ccall(:jl_gc_alloc_1w, Ptr{Void}, ())
+    end
+end
+
+# @noinline function with_gc_frame(n::Int)
+#     for s = 1:n
+#         Ref(s)
+#     end
+# end
+
+# @noinline f(a, b) = b
+
+# @noinline function unstable_no_alloc(n::Int)
+#     a = Ref(1)
+#     b = Ref(1.0)
+#     for s = 1:n
+#         a = f(a, b)
+#     end
+#     a
+# end
+
+function main()
+    @time bare_gc(2_000_000_000)
+    # @time with_gc_frame(1000_000_000)
+    # @time unstable_no_alloc(200_000_000)
+end
+
+main()

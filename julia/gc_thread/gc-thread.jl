9b8895c588d11d29731520eb8bf47ddbff35d2ab
Yichao Yu <yyc1992@gmail.com>
2016-08-01 00:24:18 +0800

    [julia/gc_thread] get rid of warnings, reproduce dead lock

--- julia/gc_thread/gc-thread.jl
+++ julia/gc_thread/gc-thread.jl
@@ -4,21 +4,26 @@ using Base.Threads
 
 srand(0)
 
-global f(x) = 1
+global f(x::Val{0}) = 1
+global f(x) = 0
+const counter = Atomic{Int}(1)
+
+println(getpid())
 
 ex = quote
     for j in 1:10
         println(j)
         @threads for i in 1:10
+            ccall(:jl_, Void, (Any,), i)
             for k in 1:10
                 r = rand() * 5
                 if r < 1
                     @gensym T
-                    eval(:(f{$T}(x::$T) = (x[]; $T)))
+                    eval(:(f{$T<:Val{$(atomic_add!(counter, 1))}}(x::$T) = $T))
                 elseif r < 2
                     gc(false)
                 elseif r < 3
-                    f(r)
+                    f(Val{rand((1:counter[]) - 1)}())
                 elseif r < 4
                     finalizer(Ref{Int}(1), f)
                 else

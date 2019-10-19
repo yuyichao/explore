1d511b8e566481a22941499fda96b46a9654cdf2
Yichao Yu <yyc1992@gmail.com>
2019-10-19 19:45:03 -0400

    [node/hello-next] Header components

--- node/hello-next/pages/about.js
+++ node/hello-next/pages/about.js
@@ -1,9 +1,12 @@
 //
 
+import Header from '../components/Header';
+
 export default function About() {
   return (
     <div>
+      <Header />
       <p>This is the about page</p>
     </div>
-  );
+  )
 }
--- node/hello-next/pages/index.js
+++ node/hello-next/pages/index.js
@@ -1,15 +1,12 @@
 //
 
-// This is the Link API
-import Link from 'next/link';
+import Header from '../components/Header';
 
-const Index = () => (
-  <div>
-    <Link href="/about">
-      <a title="About Page">About Page</a>
-    </Link>
-    <p>Hello Next.js</p>
-  </div>
-);
-
-export default Index;
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <p>Hello Next.js</p>
+    </div>
+  );
+}

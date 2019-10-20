a8576a46695e6c551ff751b854ffc98cce600fc2
Yichao Yu <yyc1992@gmail.com>
2019-10-19 20:22:15 -0400

    [node/hello-next] Child CSS

--- node/hello-next/pages/index.js
+++ node/hello-next/pages/index.js
@@ -11,17 +11,37 @@ function getPosts() {
   ];
 }
 
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
 export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
         {getPosts().map(post => (
-          <li key={post.id}>
-            <Link href="/p/[id]" as={`/p/${post.id}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
+          <PostLink key={post.id} post={post} />
         ))}
       </ul>
       <style jsx>{`

68f7617796ce70d028c72c28e900b1f4b7f91a02
Yichao Yu <yyc1992@gmail.com>
2022-10-03 14:37:36 -0400

    Debugging code

--- julia/fw_crash/crash.jl
+++ julia/fw_crash/crash.jl
@@ -8,6 +8,8 @@ function gen_fptr(::Type{T}) where {T}
     @cfunction(f, Ref{AT}, (Ref{AT},))
 end
 fptr = gen_fptr(Int)
+@show fptr
+ccall(:jl_breakpoint, Cvoid, (Ptr{Cvoid},), fptr)
 
 ccall(fptr, Ref{AT}, (Ref{AT},), [1])
 println(1)

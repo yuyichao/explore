4212b9530e956dfe800727eb9da652f80baa64ed
Yichao Yu <yyc1992@gmail.com>
2023-07-05 09:33:02 -0400

    [julia/pauli_multiply] benchmark result

--- julia/pauli_multiply/test_multiply.jl
+++ julia/pauli_multiply/test_multiply.jl
@@ -82,3 +82,51 @@ end
 for name in ARGS
     test_lib(name)
 end
+
+# Apple M1
+#   4.708 μs (0 allocations: 0 bytes)
+#   3.135 μs (0 allocations: 0 bytes)
+# GCC 12.1.0 aarch64:
+# multiply_0  4.262 μs (0 allocations: 0 bytes)
+# multiply_1  2.370 μs (0 allocations: 0 bytes)
+# multiply_1x2_1  2.148 μs (0 allocations: 0 bytes)
+# multiply_1x2_2  3.437 μs (0 allocations: 0 bytes)
+# multiply_1x4_1  2.204 μs (0 allocations: 0 bytes)
+# multiply_1x4_2  4.166 μs (0 allocations: 0 bytes)
+# multiply_2  2.666 μs (0 allocations: 0 bytes)
+# multiply_2_2  2.815 μs (0 allocations: 0 bytes)
+# multiply_3  2.958 μs (0 allocations: 0 bytes)
+# multiply_3_2  3.088 μs (0 allocations: 0 bytes)
+# Clang 15.0.7 aarch64:
+# multiply_0  4.922 μs (0 allocations: 0 bytes)
+# multiply_1  2.356 μs (0 allocations: 0 bytes)
+# multiply_1x2_1  2.134 μs (0 allocations: 0 bytes)
+# multiply_1x2_2  3.125 μs (0 allocations: 0 bytes)
+# multiply_1x4_1  2.102 μs (0 allocations: 0 bytes)
+# multiply_1x4_2  4.143 μs (0 allocations: 0 bytes)
+# multiply_2  2.662 μs (0 allocations: 0 bytes)
+# multiply_2_2  2.916 μs (0 allocations: 0 bytes)
+# multiply_3  2.958 μs (0 allocations: 0 bytes)
+# multiply_3_2  2.963 μs (0 allocations: 0 bytes)
+
+# i9-10885H
+#   7.182 μs (0 allocations: 0 bytes)
+#   3.196 μs (0 allocations: 0 bytes)
+# GCC 12.2.0 x86_64:
+# multiply_0  7.187 μs (0 allocations: 0 bytes)
+# multiply_1  1.803 μs (0 allocations: 0 bytes)
+# multiply_1x2  1.933 μs (0 allocations: 0 bytes)
+# multiply_1x4  2.158 μs (0 allocations: 0 bytes)
+# multiply_2  3.099 μs (0 allocations: 0 bytes)
+# multiply_2_2  3.060 μs (0 allocations: 0 bytes)
+# multiply_3  4.422 μs (0 allocations: 0 bytes)
+# multiply_3_2  4.385 μs (0 allocations: 0 bytes)
+# Clang 14.0.6 x86_64:
+# multiply_0  6.895 μs (0 allocations: 0 bytes)
+# multiply_1  1.965 μs (0 allocations: 0 bytes)
+# multiply_1x2  1.977 μs (0 allocations: 0 bytes)
+# multiply_1x4  2.368 μs (0 allocations: 0 bytes)
+# multiply_2  3.169 μs (0 allocations: 0 bytes)
+# multiply_2_2  3.172 μs (0 allocations: 0 bytes)
+# multiply_3  4.477 μs (0 allocations: 0 bytes)
+# multiply_3_2  4.483 μs (0 allocations: 0 bytes)

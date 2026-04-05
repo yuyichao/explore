d151f2e7c05704dac259d06f56aeb2ea22d67c5d
Yichao Yu <yyc1992@gmail.com>
2026-04-05 14:06:17 -0400

    bug

--- amaranth/transactron/method_transaction_method.py
+++ amaranth/transactron/method_transaction_method.py
@@ -9,7 +9,6 @@ class Outer(Elaboratable):
         m = TModule()
 
         inner = Method()
-        outer = Method()
 
         outer_trans_run = Signal(1)
 
@@ -17,17 +16,30 @@ class Outer(Elaboratable):
         def _():
             m.d.sync += Print("Inner method")
 
-        # trans_ready = outer.run # This fixes the issue
-        trans_ready = C(1)
+        trans_ready = Signal(1)
 
-        @def_method(m, outer, ready=C(0))
-        def _():
-            m.d.sync += Print("Outer method")
+        outer = Transaction()
+        with outer.body(m, ready=C(0)):
+            m.d.sync += Print("Outer transaction")
             outer_trans = Transaction()
             with outer_trans.body(m, ready=trans_ready):
-                m.d.sync += Print("Outer transaction")
+                m.d.sync += Print("Inner transaction")
                 inner(m)
             m.d.top_comb += outer_trans_run.eq(outer_trans.run)
+
+        # Method definition shows the same issue
+        # outer = Method()
+        # @def_method(m, outer, ready=C(0))
+        # def _():
+        #     m.d.sync += Print("Outer method")
+        #     outer_trans = Transaction()
+        #     with outer_trans.body(m, ready=trans_ready):
+        #         m.d.sync += Print("Transaction")
+        #         inner(m)
+        #     m.d.top_comb += outer_trans_run.eq(outer_trans.run)
+
+        # m.d.comb += trans_ready.eq(outer.run) # This fixes the issue
+        m.d.comb += trans_ready.eq(1)
 
         m.d.sync += Print(inner.run, outer_trans_run, outer.run)
 
